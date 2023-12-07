01d561aeece414543c7ca3a2820b9b48749d66fd
Eirik Gallefoss <eirik.gallefoss@nav.no>
2023-12-07 13:09:48 +0100

    Fjerner duplikater fra .gitignore

--- oppdater_metarepo.sh
+++ oppdater_metarepo.sh
@@ -3,3 +3,6 @@ while read -r project
 do
     meta project import "$project" git@github.com:navikt/$project.git
 done < repos.txt
+
+# Fjern duplikater fra .gitignore
+echo "$(sort -u .gitignore | cat -n | sort -nk1 | cut -f2-)" > .gitignore
