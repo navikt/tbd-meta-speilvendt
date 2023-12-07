3f4bb36d34a756ed5b0dbe6d32f9cb4b298bb736
Eirik Gallefoss <eirik.gallefoss@nav.no>
2023-12-07 12:54:34 +0100

    Riktig kommando for å importere repo

--- oppdater_metarepo.sh
+++ oppdater_metarepo.sh
@@ -1,5 +1,5 @@
 meta init --force
 while read -r project
 do
-    project import "$project" git@github.com:navikt/$project.git
+    meta project import "$project" git@github.com:navikt/$project.git
 done < repos.txt
